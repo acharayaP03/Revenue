7a39b43183e71c55aab7f8a61b439ca3af25d543
acharayaP03 <acharyaP03@gmail.com>
2025-01-09 14:38:12 +1100

    Sales revenue app with react ts amd mui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+    return (
+        <>
+            <h1>React app setup with typescript, vite and MUI for ui components</h1>
+            <div>
+                <Button variant="contained">Hello World</Button>
+            </div>
+        </>
+    );
+}
+
+export default App;
