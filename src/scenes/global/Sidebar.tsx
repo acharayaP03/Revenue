29ee94bf2b4e39627084f43b5a1ddf42ed1155e7
acharayaP03 <acharyaP03@gmail.com>
2025-02-19 21:48:04 +1100

    refactor: remove React import from Sidebar component

--- src/scenes/global/Sidebar.tsx
+++ src/scenes/global/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Sidebar as ProSidebar, Menu, MenuItem, sidebarClasses } from "react-pro-sidebar";
 
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
