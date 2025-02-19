32b1c3a654d312242d2f301f77774efa6c9c00cc
acharayaP03 <acharyaP03@gmail.com>
2025-02-19 20:04:13 +1100

    feat: add LineChart component and Line scene for displaying line charts

---
+++ src/scenes/Line/index.tsx
@@ -0,0 +1,9 @@
+import Container from "../../components/Container";
+import LineChart from "@/components/LineChart";
+export default function Bar() {
+  return (
+    <Container title="Bar Chart" subtitle="Simple Bar Chart" height="75vh">
+      <LineChart />
+    </Container>
+  );
+}
