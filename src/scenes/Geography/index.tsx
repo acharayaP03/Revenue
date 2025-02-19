ecc96e890bff034b4fdb957d76e17583d4c66d43
acharayaP03 <acharyaP03@gmail.com>
2025-02-19 20:13:33 +1100

    feat: add GeographyChart component and Geography scene for displaying geography data

---
+++ src/scenes/Geography/index.tsx
@@ -0,0 +1,10 @@
+import Container from "@/components/Container";
+import GeographyChart from "@/components/GeographyChart";
+
+export default function Geography() {
+  return (
+    <Container title="Geography Chart" subtitle="Simple Geography Chart" height="75vh">
+      <GeographyChart />
+    </Container>
+  );
+}
